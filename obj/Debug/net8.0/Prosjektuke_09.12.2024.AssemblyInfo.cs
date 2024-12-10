6485d61d105969898d24296e4a1954c06b5cc46b
Jonesey86 <irj082024@gmail.com>
2024-12-10 10:05:05 +0100

    names of elf's and assigments initial commit

--- obj/Debug/net8.0/Prosjektuke_09.12.2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/Prosjektuke_09.12.2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prosjektuke_09.12.2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ff3e32acc88f6436258f3ebb42100506dddf1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prosjektuke_09.12.2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prosjektuke_09.12.2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
