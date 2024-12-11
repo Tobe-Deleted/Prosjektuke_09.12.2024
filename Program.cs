c601ff04a97583dd2d2b8297127f5d12d185422c
amhc082024 <amhc082024@gmail.com>
2024-12-11 13:49:47 +0100

    V111224v3

--- Program.cs
+++ Program.cs
@@ -8,16 +8,47 @@ class Program
     
     {
         LoadUsers LS = new LoadUsers();
+        
+        Dictionaries dict = new Dictionaries();
+        Dictionary<string, int> cars = dict.Cars();
+        Dictionary<string, int> music = dict.Music();
+        Dictionary<string, int> streetname = dict.Streetnames();
+        List<UserInfo> humans = LS.LoadUserInfo();
+        foreach(UserInfo human in humans)
+        {
+            int moralityScore = 0;
+            moralityScore += cars[human.carModel];
+            moralityScore += streetname[human.homeAdress];
+            foreach(string genre in human.musicGenres) 
+            {
+                moralityScore += music[genre];
+            }
+            if(human.washesHands) 
+            {
+                moralityScore += 3;
+            }
+            else
+            {
+                moralityScore += -7;
+            }
+            if(human.donatesToCharity)
+            {
+                moralityScore += 5;
+            }
+            else
+            {
+                moralityScore += -1;
+            }
+            
 
-    List<UserInfo> humans = LS.LoadUserInfo();
-    Console.WriteLine(humans.Count());
-    foreach(UserInfo human in humans)
-    {
-        Console.WriteLine(human.name);
-    }
+            
+
+
+        }
+   
     
-    {
-    }
+    
+    
     }
 
 }
