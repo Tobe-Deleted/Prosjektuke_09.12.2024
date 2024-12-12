9b720d1b325469bae4cb22b2d45dbfffedfe1191
oso082024 <oso082024@gmail.com>
2024-12-12 13:46:05 +0100

    Manually merged branches

--- Funtions/Functions.cs
+++ Funtions/Functions.cs
@@ -1,4 +1,64 @@
 public class Functions()
 {
     // God kveld, Michael!
+    public void NaughtyList(List<UserInfo> naughtyList)
+    {
+        Random randomIndex = new Random();
+
+        foreach (UserInfo userinfos in naughtyList)
+        {
+            int rng = randomIndex.Next(0, 10);
+            Console.WriteLine(rng);
+            if (rng == 1)
+            {
+                // Logic..
+                userinfos.Punishment = "Gryla!";
+                Console.WriteLine($"{userinfos.Name} is eaten by {userinfos.Punishment}");
+            }
+            else
+            {
+                // Logic..
+                userinfos.Punishment = "Kull!";
+                Console.WriteLine($"{userinfos.Name} recieves {userinfos.Punishment}");
+            }
+        }
+    }
+
+    public List<Elves> CreateElves()
+    {
+        List<Elves> elves = new List<Elves>{};
+        string[] gifts = {"Helicopter made from cotton candy", "Living horse made from chocolate", "Aggressive chicken sandwich"};
+        string[] gifts2 = {"Lego castle", "Lego dragon", "Lego waterbottle"};
+        string[] gifts3 = {"Toy car", "Rubber Ducky", "Unicorn"};
+        string[] gifts4 = {"Singing doll that moonwalks", "Twerking Barbie Doll", "Dancing teletubies"};
+        string[] gifts5 = {"Vr Headset", "Gaming Pc", "Playstation 5"};
+        
+        elves.Add(new Elves {Name = "Merry Jingle", Role = "Chief elf", Gift = gifts});
+        elves.Add(new Elves {Name = "Lego lost", Role = "Lego Eater", Gift = gifts2});
+        elves.Add(new Elves {Name = "Celery Stalkinghood",  Role = "Smelling rubber,", Gift = gifts3});
+        elves.Add(new Elves {Name = "Glitterbeard the Beardless", Role = "Twerking around the christmas tree", Gift = gifts4});
+        elves.Add(new Elves {Name = "Treehuggus Maximus", Role = "Touching expensive things", Gift = gifts5});
+
+        return elves;
+    }
+
+    public void NiceList(List<UserInfo> niceList)
+    {
+        List<Elves> elves = CreateElves();
+        int elfChooser = 0;
+        foreach (UserInfo Kid in niceList)
+        {
+            if(elfChooser == 5) elfChooser = 0;
+            Kid.Elf = elves[elfChooser].Name;
+            Random rng = new Random();
+            int giftNumber = rng.Next(0, elves[elfChooser].Gift.Length);
+            Kid.Gift = elves[elfChooser].Gift[giftNumber];
+            elfChooser++;
+        } 
+        Console.WriteLine("~~Nice list~~");
+        foreach(UserInfo kid in niceList)
+        {
+            Console.WriteLine($"{kid.Name} receives {kid.Gift} from {kid.Elf}");
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,105 @@
-﻿namespace Prosjektuke_09._12._2024;
+﻿using System.Text.Json;
+
+namespace Prosjektuke_09._12._2024;
 
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello, Christian"); // du finner json filen her: /data/randomPeople.json
+        LoadUsers LS = new LoadUsers();
+        Functions fnc = new Functions();
+
+        Dictionaries dict = new Dictionaries();
+        Dictionary<string, int> cars = dict.Cars();
+        Dictionary<string, int> music = dict.Music();
+        Dictionary<string, int> streetname = dict.Streetnames();
+        List<UserInfo> humans = LS.LoadUserInfo();
+        List<UserInfo> naughtyList = new List<UserInfo>{};
+        List<UserInfo> niceList = new List<UserInfo>{};
+        foreach(UserInfo human in humans)
+        {
+            int moralityScore = 0;
+            moralityScore += cars[human.CarModel];
+            moralityScore += streetname[human.HomeAdress.TrimEnd(new char[]{'1','2','3','4','5','6','7','8','9','0',' '})];
+            foreach(string genre in human.MusicGenres) 
+            {
+                moralityScore += music[genre];
+            }
+            if(human.WashesHands) 
+            {
+                moralityScore += 3;
+            }
+            else
+            {
+                moralityScore += -7;
+            }
+            if(human.DonatesToCharity)
+            {
+                moralityScore += 5;
+            }
+            else
+            {
+                moralityScore += -1;
+            }
+            if(human.ToiletPaperOutward)
+            {
+                moralityScore += 3;
+            }
+            else
+            {
+                moralityScore += -2;
+            }
+
+            if(moralityScore<1)
+            {
+                naughtyList.Add(human);
+            }
+            else
+            {
+                niceList.Add(human);
+            }
+
+
+        }
+
+    
+    
+    
+    bool exit = false;
+    while (!exit)
+    {
+        Console.Clear();
+            Console.WriteLine("Velg hvilken liste du ønsker");
+            Console.WriteLine("1. Nicelist");
+            Console.WriteLine("2. Naughtylist");
+            Console.WriteLine("0. Avslutt");
+            ConsoleKey MainMenuChoice = Console.ReadKey().Key;
+            switch (MainMenuChoice)
+            {
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    fnc.NiceList(niceList);
+                    Console.ReadKey();
+
+                break;
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    fnc.NaughtyList(naughtyList);
+                    Console.ReadKey();
+                    break;
+
+                case ConsoleKey.D0:
+                    Console.Clear();
+                    exit = true;
+                    break;
+
+            }
     }
+   
+    
+    
+    
+    }
+
 }
